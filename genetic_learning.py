e7550d6e1420d0c33347900911b5277177e82d96
Etonado <erik-winkler@outlook.de>
2023-05-29 16:47:42 +0200

    working without plotting result

--- genetic_learning.py
+++ genetic_learning.py
@@ -1,18 +1,18 @@
 import numpy as np
-import input_generator
 import ludopy
 from multiprocessing import Pool
 import time
 
 
 
-NUMBER_OF_HIDDEN_NEURONS = 10
+NUMBER_OF_HIDDEN_NEURONS = 6
 NUMBER_OF_INPUTS = 9
 NUMBER_OF_WEIGHTS = NUMBER_OF_INPUTS * NUMBER_OF_HIDDEN_NEURONS + NUMBER_OF_HIDDEN_NEURONS
 BIT_LENGTH = 8
 GENE_LENGTH = BIT_LENGTH * NUMBER_OF_WEIGHTS
 MULTI_CORE = True 
 NUMBER_OF_CORES = 4 # can't be changed atm :/
+SINGLE_PLAYER_POSITION_OFFSET = 13 # in game field number
 
 
 
@@ -20,7 +20,7 @@ class GeneticPlayer:
 
     global GENE_LENGTH, MULTI_CORE, NUMBER_OF_CORES
 
-    def __init__(self,population_size = 100, fitness_loops = 400, mutation_rate = 0.1, crossover_rate = 0.5, selection_rate = 0.5, selection_type = "tournament"):
+    def __init__(self,population_size = 100, fitness_loops = 300, mutation_rate = 0.1, crossover_rate = 0.5, selection_rate = 0.5, selection_type = "tournament"):
         self.variants = []
         self.population_size = population_size
         self.mutation_rate = mutation_rate
@@ -28,11 +28,12 @@ class GeneticPlayer:
         self.selection_rate = selection_rate
         self.generation = 0
         self.elite_size = int(0.1*population_size)
-        self.parent_size  = int(0.2*population_size)
+        self.parent_size  = int(0.3*population_size)
         self.selection_type = selection_type
         self.tournament_size = 4
         self.fitness_loops = fitness_loops
         self.game = ludopy.Game()
+        self.input_generator = InputGenerator()
         self.__generate_initial_population()
 
     
@@ -78,7 +79,7 @@ class GeneticPlayer:
                         if len(move_pieces)>1:
                             pieces = self.game.get_pieces()
                             # generate input matrix from the current state
-                            I = input_generator.generate_inputs(player_i,pieces,mask,dice)
+                            I = self.input_generator.generate_inputs(player_i,pieces,mask,dice)
                             for idx,pieces in enumerate(move_pieces):
                                 input = I[:,pieces]
                                 activation = self.run_neural_networks(input,weights)
@@ -229,7 +230,7 @@ class GeneticPlayer:
             range = (0,self.population_size)
             self.compute_fitness(range)
 
-    def train(self,generations = 100):
+    def train(self,generations = 50):
         # compute fitness of initial population
         time_start = time.time()
         self.__call_fitness_function()
@@ -239,16 +240,17 @@ class GeneticPlayer:
         while self.generation < generations:
             # sort population
             self.__sort_population()
-            # print average fitness of previous generation
-            print("Average fitness of generation ", self.generation-1,": ", self.__average_fitness())
+            # print fitness statistics of previous generation
             # print max fitness of generation
             print("Generation: ", self.generation -1, " Max fitness: ", self.variants[0].fitness)
             # print lowest fitness of generation
             print("Generation: ", self.generation -1, " Min fitness: ", self.variants[self.population_size-1].fitness)
-
+            # print average fitness of generation
+            print("Mean and std of fitness: ", self.__mean_std_fitness())
+            mean,std = self.__mean_std_fitness()
             # save statistics
-            with open('./statistics/file1.txt', 'a') as file:
-                file.write("%d,%.2f,%.2f,%.2f\n" % (self.generation-1,self.variants[self.population_size-1].fitness,self.variants[0].fitness,self.__average_fitness()))
+            with open('./statistics/file3.txt', 'a') as file:
+                file.write("%d,%.3f,%.3f,%.3f,%.3f\n" % (self.generation-1,self.variants[self.population_size-1].fitness,self.variants[0].fitness,mean,std))
 
             best_5 = []
             for chromosome in self.variants[0:5]:
@@ -288,7 +290,7 @@ class GeneticPlayer:
 
     def get_weights_of_best_player(self):
         # open textfile to get genes of best player
-        with open('./weights/best_5_current_gen.txt', 'r') as file:
+        with open('./weights/best_current_gen.txt', 'r') as file:
             genes = np.loadtxt(file, delimiter=',', max_rows=1).astype(int)
         
                 
@@ -298,6 +300,129 @@ class GeneticPlayer:
 
         return weights
 
+
+    def evaluate_best(self,number_of_games,players = "random",plot = False):
+        
+        best_winning_rate = 0
+        best_winning_rate_index = -1
+
+        with open('./weights/best_5_current_gen.txt', 'r') as file:
+            best_genes = np.loadtxt(file, delimiter=',', max_rows=5).astype(int)
+        
+        best_variants = []
+        for genes in best_genes:
+            best_variants.append(Chromosome(genes))
+
+        
+
+        for player_index,chromosome in enumerate(best_variants):
+            # every chromosome is tested for fitness_loops times
+            weights = chromosome.decrypt_chromosome()
+
+            winning_count = 0
+            games_played = 0
+            winning_rate_over_time = []
+
+            while games_played < number_of_games:
+                self.game.reset()
+                there_is_a_winner = False
+                games_played += 1
+
+                # reset the game
+                self.game.reset()
+                # reset winner state
+                there_is_a_winner = False
+                # play the game until there is a winner
+                while not there_is_a_winner:
+                    # one game is played
+                    # reset the piece to move
+                    player_with_highest_activation = -1
+                    highest_activation = -100
+                    # get the current state of the game
+                    (dice, move_pieces, _, _, _, there_is_a_winner), player_i = self.game.get_observation()
+                    # player to be trained
+                    if player_i == 0:
+                        # set mask for all indices set in move_pieces
+                        mask = np.zeros(4)
+                        for x in move_pieces:
+                            mask[x] = 1
+                        # decision based on player state        
+                        if len(move_pieces)>1:
+                            pieces = self.game.get_pieces()
+                            # generate input matrix from the current state
+                            I = self.input_generator.generate_inputs(player_i,pieces,mask,dice)
+                            for idx,pieces in enumerate(move_pieces):
+                                input = I[:,pieces]
+                                activation = self.run_neural_networks(input,weights)
+                                if activation > highest_activation:
+                                    highest_activation = activation
+                                    player_with_highest_activation = idx
+
+                            #piece_to_move = function call
+                            if player_with_highest_activation == -1:
+                                # something went wrong
+                                print("Something went wrong with the neural network")
+                                piece_to_move = move_pieces[np.random.randint(0, len(move_pieces))]
+                            else:
+                                # move the piece with the highest activation
+                                piece_to_move = move_pieces[player_with_highest_activation] 
+
+                        elif len(move_pieces) == 1: # there is no choice for moving a piece
+                            piece_to_move = move_pieces[0]
+                        else:   # no piece can be moved
+                            piece_to_move = -1
+
+                    elif players == "random":
+                        # other players
+                        if len(move_pieces):
+                            piece_to_move = move_pieces[np.random.randint(0, len(move_pieces))]
+                        else:
+                            piece_to_move = -1
+                    else:
+                        raise Exception("Invalid player type")
+
+                    # get response from the game
+                    _, _, _, _, _, there_is_a_winner = self.game.answer_observation(piece_to_move)
+
+                if player_i == 0:
+                    winning_count +=1
+                
+                winning_rate_over_time.append(winning_count/games_played)
+
+            final_winning_rate = winning_count/games_played
+            print(int(final_winning_rate))
+            print("Player with genes ",player_index," has a winning rate of: ",final_winning_rate)
+
+            if final_winning_rate > best_winning_rate:
+                best_winning_rate_index = player_index
+                best_winning_rate = final_winning_rate
+
+        np.savetxt("./weights/best_current_gen.txt",best_variants[best_winning_rate_index].genes, delimiter=',',fmt='%1.4f',newline = ",")
+
+
+
+    def play_with_best(self,player_i,move_pieces,pieces,mask,dice):
+        # init
+        player_with_highest_activation = -1
+        highest_activation = -1000
+
+        I = self.input_generator.generate_inputs(player_i,pieces,mask,dice)
+        weights = self.get_weights_of_best_player()
+
+        for idx,pieces in enumerate(move_pieces):
+            input = I[:,pieces]
+            activation = self.run_neural_networks(input,weights)
+            if activation > highest_activation:
+                highest_activation = activation
+                player_with_highest_activation = idx
+
+        #piece_to_move = function call
+        if player_with_highest_activation == -1:
+            # something went wrong
+            raise Exception("Something went wrong with the neural network")
+        else:
+            # move the piece with the highest activation
+            return player_with_highest_activation
 
 
 class Chromosome():
@@ -354,3 +479,213 @@ class Chromosome():
 
         return np.array(weights)
     
+
+
+class InputGenerator():
+    def __init__(self,globes=True,stars=True):
+        pass
+    
+
+    def generate_inputs(self,player_i,pieces,active_player_mask, dice_roll:int):
+        
+        opponent_players = self.__get_diff_position(player_i,pieces,active_player_mask)
+        change_danger = self.__get_change_danger(opponent_players,dice_roll)
+        goal_position = self.__get_goal_position(player_i,pieces,active_player_mask)   
+        globe_position = self.__get_special_position(player_i,pieces,active_player_mask,"globe")
+        star_position = self.__get_special_position(player_i,pieces,active_player_mask,"star")
+
+        # input 0: normalized dice roll
+        i_0 = dice_roll *np.ones(4)/6
+
+        # input 1: can kick out one player
+        i_1 = np.zeros(4)
+        
+        for idx,player in enumerate(opponent_players):
+            for distances in player:
+                if distances == dice_roll:
+                    i_1[idx] = 1  
+            
+        # input_2: changes danger by moving
+
+        # positive: danger increases
+        # negative: danger decreases
+        # 0: no change
+        # danger = number of opponents that can kick out player
+        i_2 = change_danger
+        
+        # input 3: can reach home
+        i_3 = np.zeros(4)
+        for idx,distance in enumerate(goal_position):
+            if distance < dice_roll and distance != 0:
+                i_3[idx] = 1
+
+        # input 4: can reach safe zone
+        i_4 = np.zeros(4)
+        for idx,distance in enumerate(goal_position):
+            if distance-7 < dice_roll and distance-7 >= 0:
+                i_4[idx] = 1
+
+        # input 5: can reach free globe // # input 6: can reach occupied globe
+        i_5 = np.zeros(4)
+        i_6 = np.zeros(4)
+        reach_globe = np.zeros(4)
+
+        for idx,distance in enumerate(globe_position):
+            for entries in  distance:
+                if entries == dice_roll:
+                    reach_globe[idx] = 1
+        
+        i_6 = ((reach_globe == 1) & (i_1 == 1)).astype(float)
+        i_5 = ((reach_globe == 1) & (i_1 != 1)).astype(float)
+
+
+        # update i_1 for occupied globes
+        i_1 = ((reach_globe != 1) & (i_1 == 1)).astype(float)
+
+
+        # input 7: can reach star
+        i_7 = np.zeros(4)
+        for idx,distance in enumerate(star_position):
+            for entries in  distance:
+                if entries == dice_roll:
+                    i_7[idx] = 1
+
+        # input 8: normalized distance to goal
+        i_8 = goal_position/59
+
+        I = np.vstack((i_0,i_1,i_2,i_3,i_4,i_5,i_6,i_7,i_8))
+        return I
+
+    def __get_diff_position(self,player_i,pieces,mask):
+        # distance from one starting point to the next one
+        global SINGLE_PLAYER_POSITION_OFFSET
+        # extract pieces from list
+        pieces = pieces[0]
+        # get pieces of current player
+        my_pieces = pieces[player_i]
+        # Disregard pieces that are save
+        mask = np.logical_and(mask,my_pieces <(4*SINGLE_PLAYER_POSITION_OFFSET))
+        # List of opponent pieces
+        all_opponent_pieces = []
+        # Create list with entries of difference positions for all active pieces
+        player_rel_positions = np.array([[],[],[],[]])
+        # set array elements to be a list
+        player_rel_positions = player_rel_positions.tolist()
+        
+        for i in range(4):
+            # Skip the pieces from the current player
+            if i == player_i:
+                continue
+            # Get pieces of ith opponent player
+            opponent_player = pieces[i]
+            # Remove all zero entries
+            opponent_player = opponent_player[opponent_player != 0]
+            # Remove save pieces
+            opponent_player = opponent_player[opponent_player <= (4*SINGLE_PLAYER_POSITION_OFFSET+1)]
+            # Project all positions to starting point of current player
+            player_offset = (i-player_i) * SINGLE_PLAYER_POSITION_OFFSET
+            opponent_player = [x + player_offset for x in opponent_player]
+
+            # Subtract 4*player_offset from all entries that are greater than 52 to account for players that have passed the starting point of current player
+            for idx,x in enumerate(opponent_player):
+                if x > 52:
+                    opponent_player[idx] = x - 52
+
+            # Add single positions to list
+            for x in opponent_player:
+                all_opponent_pieces.append(x)
+            
+        # Players in all_opponent_pieces-list are now in the same coordinate system
+        # Add relative positions to list
+        for idx,list in enumerate(player_rel_positions):
+            if(mask[idx]==True):
+                for x in all_opponent_pieces:
+                    list.append(x-my_pieces[idx])
+
+
+        # Players that are more than half the fields away are seen from the other side
+        for player in player_rel_positions:
+            for idx,x in enumerate(player):
+                if x > 26:
+                    player[idx] = x - 52
+            '''
+                Theoretically neccessary but not needed because players with negative distances have passed the goal position and can thus not be reached by the current player
+                elif x < -26:
+                    player[idx] = x + 52
+            '''
+        # Narrow the range to [-6 and 6], other entries are discarded
+        for i in range(4):
+            player_rel_positions[i] = [x for x in player_rel_positions[i] if x >= -6 and x <= 6]
+        
+
+        return player_rel_positions
+
+
+    def __get_special_position(self,player_i,pieces,mask,type:str):
+
+        if type == "globe":
+            pos1 = 8
+            pos2 = 13
+        elif type == "star":
+            pos1 = 4
+            pos2 = 11
+        else:
+            raise ValueError("Invalid type: " + type)
+
+        global SINGLE_PLAYER_POSITION_OFFSET
+        # extract pieces from list
+        pieces = pieces[0]
+        # get pieces of current player
+        my_pieces = pieces[player_i]
+        # Disregard pieces that are save
+        mask = np.logical_and(mask,my_pieces <(4*SINGLE_PLAYER_POSITION_OFFSET))
+
+        # Create list with entries of difference positions to globes
+        special_rel_positions = np.array([[],[],[],[]])
+        # set array elements to be a list
+        special_rel_positions = special_rel_positions.tolist()
+        # Project all positions in one quarter of the board
+        my_pieces = [x % SINGLE_PLAYER_POSITION_OFFSET for x in my_pieces]
+        # Get distances for all active pieces
+        for idx,lists in enumerate(special_rel_positions):
+            if(mask[idx]==True):
+                lists.append(pos1-my_pieces[idx])
+                lists.append(pos2-my_pieces[idx])
+
+        
+        for i in range(4):
+            special_rel_positions[i] = [x for x in special_rel_positions[i] if x >= -6 and x <= 6]
+
+        return special_rel_positions
+
+    def __get_goal_position(self,player_i,pieces,mask):
+        # extract pieces from list
+        pieces = pieces[0]
+        # get pieces of current player
+        my_pieces = pieces[player_i]
+        # Disregard pieces that are save
+        goal_distance = [59 - x for x in my_pieces]
+        goal_distance = np.multiply(goal_distance,mask)
+        return goal_distance
+
+    def __get_change_danger(self,opponent_players,dice_roll):
+        # Initialize danger arrays
+        current_danger = np.zeros(4)
+        future_danger = np.zeros(4)
+        # Add danger for current position
+        for idx,player in enumerate(opponent_players):
+            for distances in player:
+                # distances between -1 and -6 might be dangerous for the current player
+                if distances <= -1 and distances >= -6:
+                    current_danger[idx] += 1
+                
+        # Add danger for future position
+        for idx,player in enumerate(opponent_players):
+            for distances in player:
+                # distances between -1 and -6 might be dangerous for the current player
+                if distances - dice_roll <= -1 and distances - dice_roll >= -6:
+                    future_danger[idx] += 1
+
+        return (future_danger-current_danger)
+
+
